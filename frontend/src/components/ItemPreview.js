cef8e71afe4e10d04e0b0cd0773ca50749fe8604
bianca <bianca@helloiconic.com>
2021-07-14 08:30:57 -0600

    Default image hange on editor and display

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import defaultImage from '../images/default.png'
+import {isUrl} from '../helpers/regex'
 
 const FAVORITED_CLASS = "btn btn-sm btn-primary";
 const NOT_FAVORITED_CLASS = "btn btn-sm btn-outline-primary";
@@ -34,10 +36,11 @@ const ItemPreview = props => {
       props.favorite(item.slug);
     }
   };
-
+  const itemImage  = isUrl(item.image) ? item.image : defaultImage;
+  const  sellerImage = isUrl(item.seller.image) ? item.seller.image : defaultImage;
   return (
     <div className="card">
-      <img src={item.image} className="card-img-top item-img" />
+      <img src={itemImage} className="card-img-top item-img" />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="preview-link">
           <h3 className="card-title">{item.title}</h3>
@@ -46,7 +49,7 @@ const ItemPreview = props => {
         <div class="d-flex flex-row align-items-center pt-2">
           <Link to={`/@${item.seller.username}`} className="flex-grow-1">
             <img
-              src={item.seller.image}
+              src={sellerImage}
               alt={item.seller.username}
               className="user-pic pr-1"
             />
